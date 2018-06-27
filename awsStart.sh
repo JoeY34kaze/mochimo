106d854c81475005c7b9d3ce61fb28f90fbdd1d2
Joey34kaze <ragnar1724@gmail.com>
2018-06-27 14:08:10 +0200

    yxc
    cxy

--- awsStart.sh
+++ awsStart.sh
@@ -7,8 +7,8 @@ chmod -R a+rwx mochimo
 
 cat << EOF > ~/runminer.sh
 #/bin/bash
-cd mochimo/mochi/bin/
-~/mochimo/mochi/bin/gomochi d -t3 >> ~/mochi.log
+cd mochimo/mochimo/mochi/bin/
+~/mochimo/mochimo/mochi/bin/gomochi d -t3 >> ~/mochi.log
 EOF
 chmod 777 ~/runminer.sh
 screen -dmS Miner -c `~/runminer.sh`
