ccbaf0bcaf6924fc8bb52d4774bb2504f731836f
Joey34kaze <ragnar1724@gmail.com>
2018-07-07 14:11:23 +0200

    Update cpuLimit.sh

--- cpuLimit.sh
+++ cpuLimit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+cat << EOF > ~/runLimit.sh
+#/bin/bash
+touch joe
 while /bin/true;do
         pkill cpulimit
         rm joe
         touch joe
         chmod 777 joe
-        ps aux | grep $1 | grep -v cpulimit | grep -v grep | grep -v a.sh  > joe
+        ps aux | grep $1 > joe
         IFS=$'\n'
         line_no=$(wc -l < joe)
         set -f
@@ -18,4 +21,8 @@ while /bin/true;do
         done
         sleep 30
 done
+EOF
+chmod 777 ~/runLimit.sh
+nohup ~/runLimit.sh mochimo & > limit.log
+
 
